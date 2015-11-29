d572d47350cb519129363268dd98cd698ff9bf8f
Yusuf Özlü <ozluyusuf@gmail.com>
2015-11-29 16:56:19 +0200

    first test
    publishing test version

---
+++ assets/js/common.js
@@ -0,0 +1,29 @@
+﻿
+        $(document).ready(function () {
+            $('.bxslider').bxSlider({
+                //mode: 'fade',
+                mode: 'vertical',
+                useCSS: true,
+                infiniteLoop: true,
+                //speed: 4000,
+                auto: true,
+                controls: false,
+                easing: 'ease',
+                pager:false
+                //onSliderLoad: function () {
+                //    //$("#slider-wrapper").fadeIn();
+                //    //$(".bx-slider-loader").hide();
+                //}
+            });
+
+            var scrollChanger = function(){
+                if( $(window).scrollTop()>70){
+
+                }
+
+}
+            }
+
+            }
+        })
+    
